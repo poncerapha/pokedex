24b9d5b197742b9fc910ad4c22cacf0b5e34b831
Raphael Ponce Da Cruz Machado Fleury <raphael.fleury@americanas.io>
2023-10-23 23:19:26 -0300

    feat: enable view binding

--- app/src/main/java/com/example/pokedex/ui/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/example/pokedex/ui/fragments/WelcomeFragment.kt
@@ -4,18 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.pokedex.R
+import com.example.pokedex.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.button.setOnClickListener {
+
+        }
     }
 }
