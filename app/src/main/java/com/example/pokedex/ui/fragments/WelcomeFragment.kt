94ea50d39521e122e00893aa760a40bc2da8f28e
Raphael Ponce Da Cruz Machado Fleury <raphael.fleury@americanas.io>
2023-10-23 23:29:54 -0300

    feat: setup all app navigation destinations

--- app/src/main/java/com/example/pokedex/ui/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/example/pokedex/ui/fragments/WelcomeFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.pokedex.R
 import com.example.pokedex.databinding.FragmentWelcomeBinding
 
@@ -25,9 +25,9 @@ class WelcomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         binding.button.setOnClickListener {
-
+            val controller = findNavController()
+            controller.navigate(R.id.searchPokemonFragment)
         }
     }
 }
